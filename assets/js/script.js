2e2f93203bddd6d30c8be41e03478ead7da18361
Valerie Gleason <valerie.gleason@gmail.com>
2022-05-26 16:03:31 -0500

    add option for multiple questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,34 @@
 //  - High scores are saved
 //  - Asnwers to questions are validated to input into high score
 
+// initialize these so I can delete them before asking the next question
+var optionsListEl = '';
+var question = '';
+var incorrectAnswer = document.createElement("p");
+
 // Initiliaze Counter variable to 30
 var timer = 30;
 var timerEl = document.querySelector("#counter-display");
 
 // TODO--Create an array of objects -- 5 'question' objects in the array to loop through
-// Store multiple-choice question as an object 
-var question1 = {
-    name: "What is the first letter of the alphabet?",
-    options: ['A','B','C','D'],
-    answer: "A"
-}
 
-var question2 = {
-    name: "What is the greatest band of all time?",
-    options: ['KISS','Boyz II Men','Chumbawumba','Josh Groban'],
-    answer: "Chumbawumba"
-}
+// Initialize the question number to start at the first question in the array
+var quesNum = 0;
+
+// Store multiple-choice questions as an array of objects
+var allQuestions = [
+    {
+        name: "What is the first letter of the alphabet?",
+        options: ['A','B','C','D'],
+        answer: "A"
+    },
+    {
+        name: "What is the greatest band of all time?",
+        options: ['KISS','Boyz II Men','Chumbawumba','Josh Groban'],
+        answer: "Chumbawumba"
+    }
+]
+
 // Set a button for the variable that starts the quiz
 var startButtonEl = document.querySelector("#start-quiz-btn");
 
@@ -33,69 +44,123 @@ var userScore = [];
 // Start quiz on button click:
 // 1. Hide the content in the quiz-intro div 
 var startQuiz = function(){
+
+    var totalNumQuestions = allQuestions.length;
+    console.log("total number of questions: " + totalNumQuestions);
+
     quizIntroEl = document.querySelector(".quiz-intro");
     quizIntroEl.setAttribute("style", "display:none");
-    askQuestion();
+
+    if (quesNum === 0) {
+        askQuestion(quesNum);
+    } else if (quesNum === allQuestions.length) {
+        console.log("Quiz is over");
+        console.log("Questions answered: " + quesNum);
+        endQuiz();
+    } else {
+        console.log("Ask the next question");
+
+        // remove existing question div
+        optionsListEl.remove();
+        question.remove();
+        askQuestion(quesNum);
+    }
 }
+
+// Check the total number of questions
+// Check the question number to ensure it's in range.
+// if not 0 and not equal to the total number of questions delete any existing question-wrapper divs
+//
+// Ask the first question & validate answer
+// Chec
 
 // 2. Display a question in the question-wrapper div
-var askQuestion = function() {
-    var question = document.createElement("h3");
-    question.textContent = question1.name;
 
-    var optionsListEl = document.createElement('ol');
-    question.setAttribute("class", "question-test");
+var askQuestion = function(num) {
+
+    // for loop to go through all the questions in the array
+
+        question = document.createElement("h3");
+
+        question.textContent = allQuestions[num].name;
+
+        optionsListEl = document.createElement('ol');
+        question.setAttribute("class", "question-test");
+
+        //add a unique id to the question so we can use it to check the correct answer later
+        allQuestions[num].id = num;
+
+        // loop through all the options in the object for the question and add them as list items
+        for (var i = 0; i < allQuestions[num].options.length; i++) {
+            // var answers = [];
+            var answer = document.createElement('li');
     
-    // loop through options in the question and add a list item for each
-    console.log(question1.options.length); //TODO -- refactor so it's looping through an array of objects
-    for (var i = 0; i < question1.options.length; i++) {
-        // var answers = [];
-        var answer = document.createElement('li');
+            // Add the options for the question to the list
+            answer.textContent = allQuestions[num].options[i];
 
-        // Add the options for the question to the list
-        answer.textContent = question1.options[i];
-
-        // set a class for a possible answer for our event listeners to know when to do something
-        answer.setAttribute("class","answer-selections");
-
-        optionsListEl.appendChild(answer);
-    }
+            // set a class for a possible answer for our event listeners to know when to do something
+            answer.setAttribute("class","answer-selections");
     
-    // write the question THEN the ol so they show up in the correct order in the html
-    questionWrapperEl.append(question);
-    questionWrapperEl.append(optionsListEl);
+            optionsListEl.appendChild(answer);
+        } // end of the for loop to append the options to the question
 
-    console.log(questionWrapperEl);
+        // write the question THEN the ol so they show up in the correct order in the html
+        questionWrapperEl.append(question);
+        questionWrapperEl.append(optionsListEl);
+        //This is where I could maybe add the getAnswerHandler?
+        questionWrapperEl.addEventListener("click", getAnswerHandler);
+        
+    
+    // finished building all questions
+
 }
-
 // function for the event listener to store the user's selection in a variable
 var getAnswerHandler = function(event) {
     var targetEl = event.target;
 
     if (targetEl.matches('.answer-selections')) {
         var answerToCheck = event.target.textContent;
+        checkAnswerHandler(answerToCheck, quesNum);
     }
     
-    checkAnswerHandler(answerToCheck);
+    
 }
 
 // 3. On click of one of the list item/answers, check if the answer is correct
-var checkAnswerHandler = function(answer) {
-    if (answer === question1.answer) {
+var checkAnswerHandler = function(answer, i) {
+    if (answer === allQuestions[i].answer) {
         console.log ("You selected the correct answer")
-        // add logic here to select the next question
+        // if the answer is correct, increment the global question number and then ask the next question
+        quesNum++;
+        console.log(quesNum);
+
+        // Ask the next question
+        startQuiz();
+
+        // Remove the incorrect answer flag
+        incorrectAnswer.remove();
     } else {
 
         timer -= 5; // Decrement timer
-        // TODO: Print "Wrong!" underneath the question
-        var incorrectAnswer = document.createElement("p");
+
+        // TODO: refactor so incorrectAnswer doesn't need to be a global variable
         incorrectAnswer.setAttribute("class", "incorrect-answer");
         incorrectAnswer.textContent = "Wrong!";
 
         questionWrapperEl.after(incorrectAnswer);
     }
 }
+var endQuiz = function() {
+    // Remove questions and answers
+    optionsListEl.remove();
+    question.remove();
 
+    // Put a placeholder in for the form to add a high score
+    var addHighScoreEl = document.createElement("p");
+    addHighScoreEl.textContent = "You finished the game! Add your high score!"
+
+    questionWrapperEl.append(addHighScoreEl);
+}
 // 4. If correct, log true to userScore; if false, display the question until the correct answer is selected (but do not log), deduct points from counter
 // 5. Loop through all the questions
 // 6. Make the counter decrement regularly and stop the game when it's at 0
@@ -123,6 +188,6 @@ var checkAnswerHandler = function(answer) {
 
 // When the user clicks the start button, start the quiz
 startButtonEl.addEventListener("click", startQuiz);
-questionWrapperEl.addEventListener("click", getAnswerHandler);
+
 
 // When the user selects an answer, store it in a variable
